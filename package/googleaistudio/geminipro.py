0fb221f8802606dd07b2f794b4e4dea5efed592a
Eliran Wong <hkwebtech@gmail.com>
2024-02-19 18:13:00 +0000

    Update geminipro.py

--- package/googleaistudio/geminipro.py
+++ package/googleaistudio/geminipro.py
@@ -46,10 +46,10 @@ class GeminiPro:
             candidate_count=1,
         )
         self.safety_settings={
-            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_ONLY_HIGH,
         }
         self.defaultPrompt = ""
         #self.enableVision = (os.path.realpath(__file__).endswith("vision.py"))
